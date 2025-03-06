03aa0b550bc6581237708e079096b2b2d1bbd286
whhoffmann <hoffmannwilliamh@gmail.com>
2025-03-06 09:34:02 +0100

    New small decision tree

--- DECISIONTREEPYTHON.py
+++ DECISIONTREEPYTHON.py
@@ -12,12 +12,12 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pylab as plt
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
 import sklearn.metrics
 
-os.chdir("C:\TREES")
+os.chdir("/home/william/Documents/git/Machine_Learning_for_Data_Analysis/")
 
 """
 Data Engineering and Analysis
@@ -37,10 +37,13 @@ Modeling and Prediction
 """
 #Split into training and testing sets
 
-predictors = data_clean[['BIO_SEX','HISPANIC','WHITE','BLACK','NAMERICAN','ASIAN',
-'age','ALCEVR1','ALCPROBS1','marever1','cocever1','inhever1','cigavail','DEP1',
-'ESTEEM1','VIOL1','PASSIST','DEVIANT1','SCHCONN1','GPA1','EXPEL1','FAMCONCT','PARACTV',
-'PARPRES']]
+# predictors = data_clean[['BIO_SEX','HISPANIC','WHITE','BLACK','NAMERICAN','ASIAN',
+# 'age','ALCEVR1','ALCPROBS1','marever1','cocever1','inhever1','cigavail','DEP1',
+# 'ESTEEM1','VIOL1','PASSIST','DEVIANT1','SCHCONN1','GPA1','EXPEL1','FAMCONCT','PARACTV',
+# 'PARPRES']]
+
+#Fewer predictors
+predictors = data_clean[['marever1', 'ALCEVR1']]
 
 targets = data_clean.TREG1
 
@@ -71,6 +74,7 @@ tree.export_graphviz(classifier, out_file=out)
 import pydotplus
 graph=pydotplus.graph_from_dot_data(out.getvalue())
 Image(graph.create_png())
+
 
 
 
